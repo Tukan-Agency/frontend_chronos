a4991f7aff121eb9a07aad8999f8808e475aacc6
Carlos de LeÓN <daniel@tukanstudios.com.gt>
2025-09-12 02:59:57 -0600

    12 de septiembre

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 
-	baseUrl: 'https://Inaqstock.com/api',
+	baseUrl: 'http://localhost:12001/api',
 };
 // baseUrl: 'https://inv.chronoswinvestment.com/api'
 // baseUrl: 'http://localhost:12001/api'
